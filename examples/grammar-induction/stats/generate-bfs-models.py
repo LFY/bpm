5b85867f1aa16c2eba3f505b607e821fdb28fd70
Ranjitha Kumar <oranju@gmail.com>
2011-12-19 19:02:57 -0800

    fixing a few bugs

--- examples/grammar-induction/stats/generate-bfs-models.py
+++ examples/grammar-induction/stats/generate-bfs-models.py
@@ -9,12 +9,12 @@ if(len(sys.argv)>2):
     probabilityThreshold = sys.argv[2]
 mode="0"
 if(len(sys.argv)>3):
-    mode = sys.argv[4]
+    mode = sys.argv[3]
 
 fileData = grammarFile.split('_')
 
 subprocess.call(["vicare","--script","../stats/generate-bfs-models.ss",grammarFile, probabilityThreshold, mode])
 
 for infile in glob.glob("bfs.dae.scene*"):
-    subprocess.call(["python","../reconst-dae.py",fileData[0]+"_"+fileData[1],infile, grammarFile.replace(".dae","").replace(".grammar.ss",".bfs")+".dae"])
+    subprocess.call(["python","../reconst-dae.py",fileData[0]+"_"+fileData[1], infile, grammarFile.replace(".dae","").replace(".grammar.ss",".bfs")+infile[-1]+".dae"])
     subprocess.call(["rm", infile])
