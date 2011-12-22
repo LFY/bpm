a049644f9c62f604437e19743746821e099df058
Ranjitha Kumar <oranju@gmail.com>
2011-12-21 21:23:46 -0800

    a few tweaks

--- examples/grammar-induction/stats/render-turk-images.py
+++ examples/grammar-induction/stats/render-turk-images.py
@@ -2,25 +2,27 @@ import glob
 import os
 import subprocess
 
-globDir = "/Users/ranju/projects/bpm/examples/grammar-induction/mturk/fodder/seuss/training/"
+globDir = "/Users/ranju/projects/bpm/examples/grammar-induction/mturk/fodder/playground/exemplars/"
+
+rigFile = "/Users/ranju/projects/bpm/examples/grammar-induction/mturk/fodder/playground/renderRig.ma"
+
 melOutName = "/Applications/Autodesk/maya2012/Maya.app/Contents/scripts/myScripts/collada2maya.mel"
 melOut = open(melOutName, 'w')
-
 melString = ""
 
-# TODO: create rig.ma file
-#if .ma files already exist don't both redoing the loading
+#load collada files into maya scenes
+#if .ma files already exist don't bother recreating them
 if (len(glob.glob(os.path.join(globDir, '*.ma')))==0):
     for infile in glob.glob(os.path.join(globDir, '*.dae')):
-        melString+="file -i \""+ infile +"\";\nfile -rename \""+infile.replace(".dae",".ma")+"\";\nfile -save -type \"mayaAscii\";\nfile -f -new;\n"
+        melString+="file -o \""+rigFile+"\"\n;file -i \""+ infile +"\";\nfile -rename \""+infile.replace(".dae",".ma")+"\";\nfile -save -type \"mayaAscii\";\nfile -f -new;\n"
 
-melOut.write(melString)
+    melOut.write(melString) 
+    melOut.close();
 
-melOut.close();
+    subprocess.call(["maya","-batch", "-command", "source \"" + melOutName + "\""])
 
-subprocess.call(["maya","-batch", "-command", "source \"" + melOutName + "\""])
-
+#render code
 for infile in glob.glob(os.path.join(globDir, '*.ma')):
     outName = infile.split('/')[-1].replace(".ma","")
     dirName = globDir.replace("fodder","images")
-    subprocess.call(["render","-r","mr","-rd",dirName,"-im",outName,"-of","png","-x","200","-y","200",infile])
+    subprocess.call(["render","-r","sw","-rd",dirName,"-im",outName,"-of","png","-x","500","-y","500","-eaa","0",infile])
