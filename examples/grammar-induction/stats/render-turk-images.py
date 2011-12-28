fd5752cf3041efbb043ca16e1d3526216ffbfc98
Ranjitha Kumar <oranju@gmail.com>
2011-12-28 15:37:38 -0800

    making it easier to run on different machines

--- examples/grammar-induction/stats/render-turk-images.py
+++ examples/grammar-induction/stats/render-turk-images.py
@@ -1,19 +1,32 @@
 import glob
 import os
 import subprocess
+import sys
 
-globDir = "/Users/ranju/projects/bpm/examples/grammar-induction/mturk/fodder/playground/exemplars/"
+# need to add GI_BASE_DIR environ variable
+# args are category, grammarType, & optionally to create maya scene files (0) or not (1)
+# grammarType: lgcg, mgcg, bayes_100_10_1.0_1.0
+category = sys.argv[1]
+grammarType = sys.argv[2]
+mode="0"
+if(len(sys.argv)>3):
+     mode = sys.argv[3]
 
-rigFile = "/Users/ranju/projects/bpm/examples/grammar-induction/mturk/fodder/playground/renderRig.ma"
+globDir = os.environ.get("GI_BASE_DIR")+"mturk/colladaMaya/"+category
 
-melOutName = "/Applications/Autodesk/maya2012/Maya.app/Contents/scripts/myScripts/collada2maya.mel"
-melOut = open(melOutName, 'w')
-melString = ""
+rigFile = globDir+"/renderRig.ma"
+
 
 #load collada files into maya scenes
-#if .ma files already exist don't bother recreating them
-if (len(glob.glob(os.path.join(globDir, '*.ma')))==0):
-    for infile in glob.glob(os.path.join(globDir, '*.dae')):
+if(mode=="0"):
+    scriptDir = os.environ.get("GI_BASE_DIR") + "stats"
+    os.putenv("MAYA_SCRIPT_PATH", scriptDir)
+    
+    melOutName = scriptDir + "/collada2maya.mel"
+    melOut = open(melOutName, 'w')  
+    melString = ""
+    
+    for infile in glob.glob(os.path.join(globDir, "*"+grammarType+"*.dae")):
         melString+="file -o \""+rigFile+"\"\n;file -i \""+ infile +"\";\nfile -rename \""+infile.replace(".dae",".ma")+"\";\nfile -save -type \"mayaAscii\";\nfile -f -new;\n"
 
     melOut.write(melString) 
@@ -22,7 +35,12 @@ if (len(glob.glob(os.path.join(globDir, '*.ma')))==0):
     subprocess.call(["maya","-batch", "-command", "source \"" + melOutName + "\""])
 
 #render code
-for infile in glob.glob(os.path.join(globDir, '*.ma')):
+for infile in glob.glob(os.path.join(globDir, "*"+grammarType+"*.ma")):
     outName = infile.split('/')[-1].replace(".ma","")
-    dirName = globDir.replace("fodder","images")
+    
+    dirName = os.environ.get("GI_BASE_DIR")+"mturk/images/"+category+"/"+grammarType
+    print dirName
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
+
     subprocess.call(["render","-r","sw","-rd",dirName,"-im",outName,"-of","png","-x","500","-y","500","-eaa","0",infile])
