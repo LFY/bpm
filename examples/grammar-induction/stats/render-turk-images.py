af5a01469b61f6c4a61c11b05f845521a4702265
Ranjitha Kumar <oranju@gmail.com>
2012-01-03 17:10:51 -0800

    basic render pipeline in place

--- examples/grammar-induction/stats/render-turk-images.py
+++ examples/grammar-induction/stats/render-turk-images.py
@@ -3,44 +3,41 @@ import os
 import subprocess
 import sys
 
-# need to add GI_BASE_DIR environ variable
-# args are category, grammarType, & optionally to create maya scene files (0) or not (1)
-# grammarType: lgcg, mgcg, bayes_100_10_1.0_1.0
-category = sys.argv[1]
-grammarType = sys.argv[2]
-mode="0"
-if(len(sys.argv)>3):
-     mode = sys.argv[3]
+grammarDir = sys.argv[1]
+flags=""
+if (len(sys.argv)>2):
+    flags = sys.argv[2]
 
-globDir = os.environ.get("GI_BASE_DIR")+"mturk/colladaMaya/"+category
+enumDir = os.path.join(grammarDir,"enumeration")
+mturkDir = os.path.join(grammarDir,"mturk-"+os.path.basename(grammarDir))
+if not os.path.exists(mturkDir):
+    os.makedirs(mturkDir)
 
-rigFile = globDir+"/renderRig.ma"
+if (len(glob.glob(os.path.join(enumDir, "*.dae")))==0 or (flags=="-c" or flags=="-cm")):
+    generate-all-collada(grammarDir)
 
-
-#load collada files into maya scenes
-if(mode=="0"):
-    scriptDir = os.environ.get("GI_BASE_DIR") + "stats"
-    os.putenv("MAYA_SCRIPT_PATH", scriptDir)
+# load collada files into maya scenes
+if (len(glob.glob(os.path.join(mturkDir, "*.ma")))==0 or (flags=="-m" or flags=="-cm")):
+    rigFile = os.path.join(grammarDir,"..","renderStage.mb")
+    rigFile = rigFile.replace("\\","/")
     
-    melOutName = scriptDir + "/collada2maya.mel"
-    melOut = open(melOutName, 'w')  
+    melOutName = os.path.join(mturkDir, "collada-to-maya.mel")
+    melOut = open(melOutName, 'w')
     melString = ""
-    
-    for infile in glob.glob(os.path.join(globDir, "*"+grammarType+"*.dae")):
-        melString+="file -o \""+rigFile+"\"\n;file -i \""+ infile +"\";\nfile -rename \""+infile.replace(".dae",".ma")+"\";\nfile -save -type \"mayaAscii\";\nfile -f -new;\n"
 
-    melOut.write(melString) 
-    melOut.close();
+    for infile in glob.glob(os.path.join(enumDir, "*.dae")):
+        infile = infile.replace("\\","/")
+        outfile = os.path.join(mturkDir,os.path.basename(infile.replace("dae","mb")))
+        outfile.replace("\\","/")
+        melString+="file -o \""+rigFile+"\"\n;file -i \""+ infile +"\";\nfile -rename \""+outfile+"\";\nfile -save -type \"mayaAscii\";\nfile -f -new;\n"
 
+    melOut.write(melString)
+    melOut.close()
+        
     subprocess.call(["maya","-batch", "-command", "source \"" + melOutName + "\""])
 
 #render code
-for infile in glob.glob(os.path.join(globDir, "*"+grammarType+"*.ma")):
-    outName = infile.split('/')[-1].replace(".ma","")
-    
-    dirName = os.environ.get("GI_BASE_DIR")+"mturk/images/"+category+"/"+grammarType
-    print dirName
-    if not os.path.exists(dirName):
-        os.makedirs(dirName)
-
-    subprocess.call(["render","-r","sw","-rd",dirName,"-im",outName,"-of","png","-x","500","-y","500","-eaa","0",infile])
+for infile in glob.glob(os.path.join(mturkDir, "*.ma")):
+    outName = os.path.basename(infile).replace(".ma","")
+ 
+    subprocess.call(["render","-r","sw","-rd",mturkDir,"-im",outName,"-of","png","-x","500","-y","500","-eaa","0",infile])
